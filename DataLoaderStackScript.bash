7b15af76a1336cf7b26b3b524cb1d066ca2ae52d
Mike Ostenberg <mostenbe@akamai.com>
2023-04-07 16:54:02 -0700

    bash changed echo to wall command.

--- DataLoaderStackScript.bash
+++ DataLoaderStackScript.bash
@@ -18,7 +18,7 @@ sudo apt install ufw -y
 sudo ufw allow ssh
 sudo ufw allow https
 sudo ufw allow http
-sudo ufw -force enable
+sudo ufw –force enable
 sudo ufw status
 ##Install certbot, which relies on snapd package manager
 sudo apt install snapd -y
@@ -31,7 +31,6 @@ sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 ## Store IPV4 address in an env variable MYIP
 export MYIP=$(hostname -I | awk '{print $1}')
-
 #Some functions to lookup reverse DNS
 function system_primary_ip {
     local ip_address="$(ip a | awk '/inet / {print $2}')"
@@ -57,11 +56,11 @@ function get_rdns_primary_ip {
 sudo certbot --apache --email=$CERT_EMAIL --non-interactive --agree-tos --domain=$(get_rdns_primary_ip)
 ## In Apache, enable the cgi module to allow python processing
 sudo a2enmod cgid
-## Restart Apache2
-sudo systemctl restart apache2
 ## Modify the apache config files to allow python files to execute as cgi scripts
 sed -i 's|ScriptAlias /cgi-bin/ .*|ScriptAlias /py/ /var/www/py/| ; s|OwnerMatch|OwnerMatch\n\t\t\tAddHandler cgi-script .cgi .py| ; s|<Directory "/usr/lib/cgi-bin">|<Directory "/var/www/py">|' /etc/apache2/conf-available/serve-cgi-bin.conf
 sudo mkdir /var/www/py
+## Restart Apache2
+sudo systemctl restart apache2
 #Add the data loader python code which will generate dummy data and send to the elastic server
 cd /var/www/py
 wget https://raw.githubusercontent.com/mostenberg/DS2-DataLoader/master/py/sendBulkDSData.py
@@ -78,7 +77,6 @@ chmod g+w /var/www/py/bulkFile.txt
 #Download the data loader web page from github
 cd /var/www/html
 wget https://raw.githubusercontent.com/mostenberg/DS2-DataLoader/master/html/ds2.html
-#Wait 10 sec then restart Apache2
-sleep 10
-sudo systemctl restart apache2
-echo "Instance setup is complete. Please go to browser to pull up webpage or go to <yourdomain>/ds2.html to pull up data loader page."
+#Print out final message indicating completion
+wall "Instance setup is complete. Please go to browser to pull up webpage or go to <yourdomain>/ds2.html to pull up data loader page."
+echo "Installation is complete."
