000d46476fbc486c5ac3d5843081a86f8d15279b
Mike Ostenberg <mostenbe@akamai.com>
2023-04-19 09:52:27 -0700

    Rollback - removed CMCD query strings as

---
+++ py/sendBulkDSData-tester.py
@@ -0,0 +1,11 @@
+import sendBulkDSData
+
+
+#def sendDataToElastic(url,username,password,count,jsonData):
+
+url="http://170-187-149-159.ip.linodeusercontent.com:5601/datastream2/_bulk"
+username = "ds2user"
+password = "u34aB6RsO4I4%Uc2qYNGnVS4vlKpvA"
+count =1000
+
+sendDataToElastic(url,username,password,count,)
